ed7c82ea1df06f2172e1b2183c033f89a311b507
Michiel Gerritsen <michiel@controlaltdelete.nl>
2024-06-24 13:37:17 +0200

    Feature: Add methods

--- renderers.js
+++ renderers.js
@@ -9,8 +9,10 @@ if (useComponents) {
 }
 
 export default {
+  mollie_methods_alma: DefaultRenderer,
   mollie_methods_applepay:
     applePayType === 'direct' ? ApplePayRenderer : DefaultRenderer,
+  mollie_methods_bancomatpay: DefaultRenderer,
   mollie_methods_bancontact: DefaultRenderer,
   mollie_methods_banktransfer: DefaultRenderer,
   mollie_methods_belfius: DefaultRenderer,
@@ -28,11 +30,12 @@ export default {
   mollie_methods_klarnapaylater: DefaultRenderer,
   mollie_methods_klarnapaynow: DefaultRenderer,
   mollie_methods_klarnasliceit: DefaultRenderer,
-  mollie_methods_voucher: DefaultRenderer,
   mollie_methods_mybank: DefaultRenderer,
   mollie_methods_paypal: DefaultRenderer,
   mollie_methods_paysafecard: DefaultRenderer,
   mollie_methods_przelewy24: DefaultRenderer,
   mollie_methods_sofort: DefaultRenderer,
   mollie_methods_twint: DefaultRenderer,
+  mollie_methods_riverty: DefaultRenderer,
+  mollie_methods_voucher: DefaultRenderer,
 };
